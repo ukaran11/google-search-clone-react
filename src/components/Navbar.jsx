6f96348756e9aaa68eeaa5ca06b6ec72f427fdaf
Utsav Karan <ukaran11@gmail.com>
2021-10-25 16:59:17 +0530

    Utsav | Enabled dark mode toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-export const Navbar = () => {
+export const Navbar = ({ darkTheme, setDarkTheme}) => {
     return (
         <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
            <div className="flex justify-between items-center space-x-5 w-screen">
@@ -10,6 +10,9 @@ export const Navbar = () => {
                  Goggl 🔎
                  </p>
              </Link>
+             <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">
+                {darkTheme ? '💡 Light' : '🌙 Dark'}
+             </button>
            </div>
         </div>
     )
