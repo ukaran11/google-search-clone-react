fb0fff8d7a001a231c91fdedc1a041ef259ac48b
Utsav Karan <ukaran11@gmail.com>
2021-10-26 15:35:54 +0530

    Utsav | Added the ResultContextProvider

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,29 @@
+import React, { createContext, useContext, useState} from 'React';
+
+const ResultContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const ResultContextProvider = ({ children }) => {
+    const [results, setResults] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    // /videos /search /images
+    const getResults = async (type) => {
+        setIsLoading(true);
+
+        const response = await fetch(`${baseUrl}${type}`, {
+            method: 'GET',
+            headers: {
+                'x-user-agent': 'desktop',
+                'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+                'x-rapidapi-key': '43aedcd373msh75ecc7e0896fec7p15ca09jsn15efb283dbc7'
+              }
+        });
+
+        const data = await response.json();
+
+        setResults(data);
+        setIsLoading(false);
+    }
+}
