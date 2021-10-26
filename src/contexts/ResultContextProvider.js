f58fe38ab0bea4fa7f9dff6e8391555b60eea6fa
Utsav Karan <ukaran11@gmail.com>
2021-10-26 16:11:44 +0530

    Utsav | ResultContextProvider

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -26,4 +26,12 @@ export const ResultContextProvider = ({ children }) => {
         setResults(data);
         setIsLoading(false);
     }
+
+    return (
+        <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}>
+            {{ children }}
+        </ResultContext.Provider>
+    )
 }
+
+export const useResultContext = () => useContext(ResultContext);
