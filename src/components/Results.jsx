d0acef6b5e5705323f798645525d288e12a99584
Utsav Karan <ukaran11@gmail.com>
2021-10-30 19:09:34 +0530

    Utsav | Rendered link and title

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -9,11 +9,34 @@ const Results = () => {
     const {results, isLoading, getResults, searchTerm} = useResultContext();
     const location = useLocation();
 
+    useEffect(() => {
+        getResults('/search/q=JavaScript&num=40')
+    }, []);
+
     if(isLoading) return <Loading />
     console.log(location.pathname);
 
     switch(location.pathname) {
         case '/search':
+            return (
+                <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
+                    {results?.results?.map(({link, title}) => (
+                        <a href={link} target="_blank" rel="noreferrer">
+                            <p className="text-sm">
+                                {link.length > 30 ? link.substring(0, 30) : link}
+                            </p>
+                            <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
+                                { title }
+                            </p>
+                        </a>
+                    ))}
+                </div>
+            )
+        case '/images':
+            return 'SEARCH';
+        case '/news':
+            return 'SEARCH';
+        case '/videos':
             return 'SEARCH';
         
         default:
